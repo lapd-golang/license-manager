96b99888985c0722e8c3ef5d655d89057d78eb00
sevren <kgoguev@gmail.com>
2019-04-20 21:01:47 +0200

    added logic to save license from the rabbitmq to the database table

--- db/client.go
+++ db/client.go
@@ -5,14 +5,31 @@ import (
 
 	"github.com/jinzhu/gorm"
 	"github.com/sevren/test/models"
+	log "github.com/sirupsen/logrus"
 )
 
 type Dao struct {
 	DB *gorm.DB
 }
 
-func (d *Dao) StoreUsedLicenses(license string) {
+//StoreUsedLicenses - Stores each license recieved from the rabbitMQ exchange
+func (d *Dao) StoreUsedLicenses(license string) error {
 
+	l := models.Used_licenses{License: license}
+	var res *gorm.DB
+	db := d.DB.Table("used_licenses")
+
+	// Check if the database already contains that license
+	if db.Where("license = ?", license).RecordNotFound() {
+		res = db.Create(l)
+
+		if res.Error != nil {
+			log.Error(res.Error)
+			return res.Error
+		}
+	}
+
+	return nil
 }
 
 //GetLicenses - Retrieves the Users licenses from the database
